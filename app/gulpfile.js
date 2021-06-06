7f7ef855e5b283eb6033313c5358e0b6337a9435
Vineet Venugopal <vineet142008@gmail.com>
2021-06-06 19:40:50 +0530

    'Update'

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,41 +1,24 @@
-const { series } = require('gulp');
+const { task, series, src, dest } = require('gulp');
 const fs = require("fs")
 const path = require("path")
 const childProc = require('child_process');
 
-/**
- * Look ma, it's cp -R.
- * @param {string} src  The path to the thing to copy.
- * @param {string} dest The path to the new copy.
- */
-var copyRecursiveSync = function(src, dest) {
-  var exists = fs.existsSync(src);
-  var stats = exists && fs.statSync(src);
-  var isDirectory = exists && stats.isDirectory();
-  if (isDirectory) {
-	if (!fs.existsSync(dest)) fs.mkdirSync(dest);
-	
-    fs.readdirSync(src).forEach(function(childItemName) {
-      copyRecursiveSync(path.join(src, childItemName),
-                        path.join(dest, childItemName));
-    });
-  } else {
-    fs.copyFileSync(src, dest);
-  }
-};
-
 function copyBuildFiles(cb) {
   
-  copyRecursiveSync('build', '../')
+  //copyRecursiveSync('build', '../')
+  
+  src('build/**').pipe(dest('../'));
   
   cb();
 }
 
-function commitWebPage(cb) {
+function publish(cb) {
 	
-	childProc.execSync("git add .. && git commit -m 'Update'");
+	childProc.execSync("git add .. && git commit -m 'Update' && git push");
 	
 	cb();
 }
 
-exports.default = series(copyBuildFiles, commitWebPage);
+exports.copyBuildFiles = copyBuildFiles;
+exports.publish = publish;
+exports.default = series(copyBuildFiles, publish);
